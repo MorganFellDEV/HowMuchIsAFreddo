459fbdb33a74c3fb8a7f241cb16355e63300f647
Morgan Fell <morganfell@icloud.com>
2021-06-05 16:20:04 +0100

    Modified Tesco scraper User Agent.

--- hmiaf-scrape/scraperSettings.py
+++ hmiaf-scrape/scraperSettings.py
@@ -7,7 +7,7 @@ gb_headers = {
 }
 
 tesco_headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.2 Safari/605.1.15',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding':'gzip, deflate, br',
